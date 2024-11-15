5d1cc70582dec8cb8d64adc05953df6fb427fa45
cristian-95 <nem.cristianrc@gmail.com>
2024-11-15 19:04:59 -0300

    atualizando symtab conforme feito em aula

--- symtab.c
+++ symtab.c
@@ -13,61 +13,91 @@
 #include <string.h>
 #include <symtab.h>
 
-double vmem[SYMTABSIZE];
-char symtab[SYMTABSIZE][MAXIDLEN + 1];
-int symtab_next_entry = 0;
+#define MAXSTBSIZE 0x100000
 
-/*
-    lookup procura um nome na tabela de simbolos e retorna seu indice, caso não encontrar, retorna -1
-*/
-int lookup(char *varname)
+SYMTAB symtab[MAXSTBSIZE];
+int symtab_next_entry = 0;
+// double vmem[SYMTABSIZE];
+// char symtab[SYMTABSIZE][MAXIDLEN + 1];
+
+// /*
+//     lookup procura um nome na tabela de simbolos e retorna seu indice, caso não encontrar, retorna -1
+// */
+// int lookup(char *varname)
+// {
+//     int i = 0;
+//     for (i = 0; i < symtab_next_entry; i++)
+//     {
+//         if (strcmp(varname, symtab[i]) == 0)
+//             break;
+//     }
+//     if (i == symtab_next_entry)
+//     {
+//         return -1; // no achou varname em symtab
+//     }
+//     return i;
+// }
+
+// /*
+//     store registra uma nova variavel armazenando seu nome em symtab e seu valor em vmem
+// */
+// void store(char *varname, double value)
+// {
+//     int i = lookup(varname);
+
+//     if (i < 0)
+//     {
+//         i = symtab_next_entry;
+//         strcpy(symtab[i], varname);
+//         vmem[i] = value;
+//     }
+//     else
+//     {
+//         vmem[i] = value;
+//     }
+//     symtab_next_entry++;
+// }
+
+// /*
+//     recall procura e retorna o valor de uma váriavel
+// */
+// double recall(char *varname)
+// {
+//     int i = lookup(varname);
+
+//     if (i < 0)
+//     {
+//         return 0.0;
+//     }
+//     else
+//     {
+//         return vmem[i];
+//     }
+// }
+
+// looks up a symbol append to the symbol table (symtab)
+extern int symtab_lookup(char const *lexeme, int lexlevel)
 {
     int i = 0;
     for (i = 0; i < symtab_next_entry; i++)
     {
-        if (strcmp(varname, symtab[i]) == 0)
-            break;
+        if (strcmp(lexeme, symtab[i].name) == 0 && symtab[i].lexlevel == lexlevel)
+            return i;
     }
-    if (i == symtab_next_entry)
-    {
-        return -1; // no achou varname em symtab
-    }
-    return i;
-}
-
-/*
-    store registra uma nova variavel armazenando seu nome em symtab e seu valor em vmem
-*/
-void store(char *varname, double value)
+    return -1;
+};
+// looks up a predefined
+extern int symtab_append(char const *lexeme, int lexlevel)
 {
-    int i = lookup(varname);
-
-    if (i < 0)
+    if (symtab_lookup(lexeme, lexlevel) == -1)
     {
-        i = symtab_next_entry;
-        strcpy(symtab[i], varname);
-        vmem[i] = value;
+        SYMTAB newSymbol = {lexeme, 0, 0, lexlevel, 0};
+        symtab[symtab_next_entry] = newSymbol;
+        symtab_next_entry++;
+        return 0;
     }
     else
     {
-        vmem[i] = value;
+        return -1;
     }
-    symtab_next_entry++;
-}
-
-/*
-    recall procura e retorna o valor de uma váriavel
-*/
-double recall(char *varname)
-{
-    int i = lookup(varname);
-
-    if (i < 0)
-    {
-        return 0.0;
-    }
-    else
-    {
-        return vmem[i];
-    }
-}
+};
--- symtab.h
+++ symtab.h
@@ -10,15 +10,30 @@
  * João Pedro Brum Terra
  *
  ***************************************************/
+#pragma once
 #include <constants.h>
 
-int lookup(char *varname);
-void store(char *varname, double value);
-double recall(char *varname);
+// int lookup(char *varname);
+// void store(char *varname, double value);
+// double recall(char *varname);
+// /* memória da calculadora */
+// extern double vmem[];
+// /* tabela de simbolos */
+// extern char symtab[SYMTABSIZE][MAXIDLEN + 1];
+// /* proximo item da tabela */
+// extern int symtab_next_entry;
 
-/* memória da calculadora */
-extern double vmem[];
-/* tabela de simbolos */
-extern char symtab[SYMTABSIZE][MAXIDLEN + 1];
-/* proximo item da tabela */
-extern int symtab_next_entry;
+typedef struct _symtab_
+{
+    char *name[MAXIDLEN];
+    int objtype; // objtype pode ser == 0 if procedure; == 1 if function; == 2 if variable
+    int type;    // type == 0 if int; type == 1 if int64; == 2 if float32; == 3 if float64; == 4 if bool
+    int lexlevel;
+    int parmflag; // == 0 if variable; == 1 if parameter
+} SYMTAB;
+
+extern SYMTAB symtab[];
+// looks up a symbol append to the symbol table (symtab)
+extern int symtab_lookup(char const *, int);
+// looks up a predefined
+extern int symtab_append(char const *, int);
